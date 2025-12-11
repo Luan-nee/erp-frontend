cc25a6dec801755f203d0bc0ded8521a766dec04
Luan-nee <luandelsol54@gmail.com>
2025-12-11 14:13:22 -0500

    agrendo diseño a los MetricCard cuando están cargando o hay error

--- src/components/MetricCard.tsx
+++ src/components/MetricCard.tsx
@@ -1,22 +1,71 @@
+import Loading from "../animation/Loading";
+import { XCircle, CheckCircle } from 'lucide-react'; // Importamos iconos para el estado de error y el valor por defecto
+
 interface MetricCardProps {
   name: string;
-  value: number; 
+  value: number;
   color: string;
+  isError: boolean;
+  isLoading: boolean;
   children?: React.ReactNode;
 }
 
-export default function MetricCard({ name, value, color, children }: MetricCardProps) {
+export default function MetricCard({ 
+  name, 
+  value, 
+  color, 
+  isError, 
+  isLoading, 
+  children 
+}: MetricCardProps) {
+
+  // --- 1. Definición de Clases Base para Tailwind ---
+  const baseClasses = `bg-gradient-to-br rounded-xl p-6 transition-all duration-300`;
+  const bgClasses = `from-${color}-900/50 to-${color}-800/30 border border-${color}-700/50`;
+  
+  // --- 2. Manejo del Estado de Carga (isLoading) ---
+  if (isLoading) {
+    return (
+      <div className={`${baseClasses} ${bgClasses} flex items-center justify-center h-32`}>
+        {/* Usamos el componente Loading con el color de la tarjeta */}
+        <Loading w={8} h={8} color={color} />
+        <p className={`ml-3 text-${color}-300 font-medium`}>Cargando...</p>
+      </div>
+    );
+  }
+
+  // --- 3. Manejo del Estado de Error (isError) ---
+  if (isError) {
+    // Usamos clases de color rojo/error para indicar el fallo
+    return (
+      <div className={`${baseClasses} bg-red-900/50 border border-red-700/50 flex items-center h-32`}>
+        <div className="flex items-center">
+          <XCircle className="w-8 h-8 text-red-400 mr-4" />
+          <div>
+            <p className="text-red-300 text-sm font-medium mb-1">
+              {name}
+            </p>
+            <p className="text-xl font-bold text-white">Error de Carga</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  // --- 4. Renderizado Normal (Éxito) ---
   return (
-    <div className={`bg-gradient-to-br from-${color}-900/50 to-${color}-800/30 border border-${color}-700/50 rounded-xl p-6` }>
+    <div className={`${baseClasses} ${bgClasses}`}>
       <div className="flex items-center justify-between">
         <div>
           <p className={`text-${color}-300 text-sm font-medium mb-1`}>
             {name}
           </p>
+          {/* El valor solo se muestra si NO está cargando y NO hay error */}
           <p className="text-3xl font-bold text-white">{value}</p>
         </div>
+        {/* El children (normalmente un icono) se muestra solo en el estado de éxito */}
         <div className={`w-12 h-12 bg-${color}-600 rounded-lg flex items-center justify-center`}>
-          {children}
+          {children || <CheckCircle className="w-6 h-6 text-white" />}
         </div>
       </div>
     </div>
--- src/pages/categorias/Categoria.tsx
+++ src/pages/categorias/Categoria.tsx
@@ -19,6 +19,7 @@ import useFetcher from "../../data/useFetcher";
 import FormCreate from "./FormCreate";
 import FormEdit from "./FormEdit";
 import FormDelete from "./FormDelete";
+import Loading from "../../animation/Loading";
 
 function Categories() {
   const [showCreateModal, setShowCreateModal] = useState(false);
@@ -26,21 +27,19 @@ function Categories() {
   const [showDeleteModal, setShowFormDelete] = useState(false);
 
   const [idCategoriaEdit, setIdCategoriaEdit] = useState<number | null>(null);
-  const [idCategoriaDelete, setIdCategoriaDelete] = useState<number | null>(
-    null
-  );
+  const [idCategoriaDelete, setIdCategoriaDelete] = useState<number | null>(null);
 
   // capturar las acciones de los formularios
-  const [action, setAction] = useState(false);
+  const [recargarPagina, setRecargarPagina] = useState(false);
   useEffect(() => {
-    if (action) {
+    if (recargarPagina) {
       // lógica para recargar los datos, por ejemplo, volver a llamar a useFetcher o actualizar el estado
       // ...
       // luego de recargar los datos, restablecer action a false
-      setAction(false);
+      setRecargarPagina(false);
       console.log("Recargando datos de categorías...");
-    } 
-  }, [action]);
+    }
+  }, [recargarPagina]);
 
   const [searchTerm, setSearchTerm] = useState("");
 
@@ -113,71 +112,101 @@ function Categories() {
           <div className="bg-gray-800 rounded-xl shadow-2xl border border-gray-700 overflow-hidden">
             <div className="overflow-x-auto">
               <Table headerTable={headerTable}>
-                {filteredCategories.map((category) => (
-                  <tr
-                    key={category.id}
-                    className={`hover:bg-gray-750 transition-colors bg-gray-825`}
-                  >
-                    <td className="px-6 py-4">
-                      <div className="w-10 h-10 bg-gradient-to-br from-red-600 to-red-700 rounded-lg flex items-center justify-center shadow-lg">
-                        <FolderOpen className="w-5 h-5 text-white" />
-                      </div>
-                    </td>
-                    <td className="px-6 py-4">
-                      <div className="flex items-center gap-3">
-                        <div>
-                          <div className="text-white font-semibold text-lg">
-                            {category.nombre}
-                          </div>
-                          <div className="text-xs text-gray-400 mt-1">
-                            ID:{category.id}
-                          </div>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="px-6 py-4">
-                      <p className="text-gray-300 text-sm">
-                        {category.descripcion}
+                {categoriesLoading ? (
+                  <tr>
+                    <td
+                      colSpan={headerTable.length}
+                      className="flex flex-row items-center justify-center py-6"
+                    >
+                      <p className="px-6 py-4 text-center text-gray-400">
+                        Cargando categorías...  
                       </p>
-                    </td>
-                    <td className="px-6 py-4 text-center">
-                      <div className="flex items-center justify-center gap-2">
-                        <Tag className="w-4 h-4 text-red-400" />
-                        <span className="inline-flex items-center px-4 py-2 rounded-full text-sm font-bold bg-blue-500/20 text-blue-400 border border-blue-500/30">
-                          {category.cantidad_productos}
-                        </span>
-                      </div>
-                    </td>
-                    <td className="px-6 py-4">
-                      <div className="flex items-center justify-center gap-2">
-                        <button
-                          onClick={() => {
-                            setShowFormModal(true);
-                            setIdCategoriaEdit(category.id);
-                          }}
-                          className="p-2 bg-blue-600 hover:bg-blue-700 rounded-lg text-white transition-colors shadow-md group relative"
-                        >
-                          <Edit3 className="w-4 h-4" />
-                          <span className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-gray-900 text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap">
-                            Editar
-                          </span>
-                        </button>
-                        <button
-                          onClick={() => {
-                            setShowFormDelete(true);
-                            setIdCategoriaDelete(category.id);
-                          }}
-                          className="p-2 bg-red-600 hover:bg-red-700 rounded-lg text-white transition-colors shadow-md group relative"
-                        >
-                          <Trash2 className="w-4 h-4" />
-                          <span className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-gray-900 text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap">
-                            Eliminar
-                          </span>
-                        </button>
-                      </div>
+                      <Loading w={8} h={8} color="red" />
                     </td>
                   </tr>
-                ))}
+                ) : categoriesError ? (
+                  <tr>
+                    <td
+                      colSpan={headerTable.length}
+                      className="px-6 py-4 text-center text-red-500"
+                    >
+                      Error al cargar las categorías.{" "}
+                      <button
+                        onClick={() => setRecargarPagina(true)}
+                        className="underline text-red-400 hover:text-red-600"
+                      >
+                        Recargar página
+                      </button>
+                    </td>
+                    
+                  </tr>
+                ) : (
+                  filteredCategories.map((category) => (
+                    <tr
+                      key={category.id}
+                      className={`hover:bg-gray-750 transition-colors bg-gray-825`}
+                    >
+                      <td className="px-6 py-4">
+                        <div className="w-10 h-10 bg-gradient-to-br from-red-600 to-red-700 rounded-lg flex items-center justify-center shadow-lg">
+                          <FolderOpen className="w-5 h-5 text-white" />
+                        </div>
+                      </td>
+                      <td className="px-6 py-4">
+                        <div className="flex items-center gap-3">
+                          <div>
+                            <div className="text-white font-semibold text-lg">
+                              {category.nombre}
+                            </div>
+                            <div className="text-xs text-gray-400 mt-1">
+                              ID:{category.id}
+                            </div>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="px-6 py-4">
+                        <p className="text-gray-300 text-sm">
+                          {category.descripcion}
+                        </p>
+                      </td>
+                      <td className="px-6 py-4 text-center">
+                        <div className="flex items-center justify-center gap-2">
+                          <Tag className="w-4 h-4 text-red-400" />
+                          <span className="inline-flex items-center px-4 py-2 rounded-full text-sm font-bold bg-blue-500/20 text-blue-400 border border-blue-500/30">
+                            {category.cantidad_productos}
+                          </span>
+                        </div>
+                      </td>
+                      <td className="px-6 py-4">
+                        <div className="flex items-center justify-center gap-2">
+                          <button
+                            onClick={() => {
+                              setShowFormModal(true);
+                              setIdCategoriaEdit(category.id);
+                            }}
+                            className="p-2 bg-blue-600 hover:bg-blue-700 rounded-lg text-white transition-colors shadow-md group relative"
+                          >
+                            <Edit3 className="w-4 h-4" />
+                            <span className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-gray-900 text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap">
+                              Editar
+                            </span>
+                          </button>
+                          <button
+                            onClick={() => {
+                              setShowFormDelete(true);
+                              setIdCategoriaDelete(category.id);
+                            }}
+                            className="p-2 bg-red-600 hover:bg-red-700 rounded-lg text-white transition-colors shadow-md group relative"
+                          >
+                            <Trash2 className="w-4 h-4" />
+                            <span className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-gray-900 text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap">
+                              Eliminar
+                            </span>
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  ))
+                )}
               </Table>
             </div>
           </div>
@@ -186,9 +215,9 @@ function Categories() {
           <div className="grid grid-cols-3 gap-6 mt-8">
             <MetricCard
               name="Total Categorías"
-              value={
-                (resumen as PropResumenCategoria[])[0]?.total_categorias || 0
-              }
+              isError={resumenError} 
+              isLoading={resumenLoading} 
+              value={(resumen as PropResumenCategoria[])[0]?.total_categorias}
               color="red"
             >
               <FolderOpen className="w-6 h-6 text-white" />
@@ -196,9 +225,9 @@ function Categories() {
 
             <MetricCard
               name="Total Productos"
-              value={
-                (resumen as PropResumenCategoria[])[0]?.total_productos || 0
-              }
+              isError={resumenError} 
+              isLoading={resumenLoading} 
+              value={(resumen as PropResumenCategoria[])[0]?.total_productos}
               color="green"
             >
               <Package className="w-6 h-6 text-white" />
@@ -206,9 +235,9 @@ function Categories() {
 
             <MetricCard
               name="Promedio por Categoría"
-              value={
-                (resumen as PropResumenCategoria[])[0]?.promedio_categoria || 0
-              }
+              isError={resumenError} 
+              isLoading={resumenLoading} 
+              value={(resumen as PropResumenCategoria[])[0]?.promedio_categoria}
               color="blue"
             >
               <BarChart3 className="w-6 h-6 text-white" />
@@ -218,11 +247,16 @@ function Categories() {
       </div>
 
       {/* Create Category Modal */}
-      {showCreateModal && <FormCreate setShowFormCreate={setShowCreateModal} setAction={setAction} />}
+      {showCreateModal && (
+        <FormCreate
+          setShowFormCreate={setShowCreateModal}
+          setAction={setRecargarPagina}
+        />
+      )}
 
       {showEditModal && (
         <FormEdit
-          setAction={setAction}
+          setAction={setRecargarPagina}
           setShowFormEdit={setShowFormModal}
           dataCategoria={
             (categories as PropCategoria[]).find(
@@ -234,7 +268,7 @@ function Categories() {
 
       {showDeleteModal && (
         <FormDelete
-          setAction={setAction} 
+          setAction={setRecargarPagina}
           setShowFormDelete={setShowFormDelete}
           dataCategoria={
             (categories as PropCategoria[]).find(
